1f19260b68e768ee331fceb3fcb7f9aa3e9985fa
IDGS-803-20000803 <daniel.abraham.sanchez.aboytes@gmail.com>
2023-11-06 18:44:48 -0600

    Pagina de Inicio

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
-
+  linksMenu: Array<any> = [];
+  constructor(){}
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.linksMenu = [
+      {
+        nombre:'Inicio',
+        icono:'uil uil-building',
+        router:['/']
+      },
+      {
+        nombre:'¿Quiénes Somos',
+        icono:'uil uil-comment-question',
+        router:['/']
+      },
+      {
+        nombre:'Normativa',
+        icono:'uil uil-file-bookmark-alt',
+        router:['/']
+      },
+      {
+        nombre:'Contactanos',
+        icono:'uil uil-envelope-minus',
+        router:['/']
+      },
+      {
+        nombre: 'Noticias',
+        icono:'uil uil-newspaper',
+        router:['/']
+      }
+    ];
+  }
 }
